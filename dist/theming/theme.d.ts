eb4be8c3b6599512f5be2a623bd518f836b3d206
Blaine F Garrett <blaine@blainegarrett.com>
2020-07-23 18:25:07 -0500

    RC 1.0.14

--- dist/theming/theme.d.ts
+++ dist/theming/theme.d.ts
@@ -1,2 +1,54 @@
+export interface VenueCategoryStyle {
+    default_icon: string;
+    premium_icon: string;
+    default_color: string;
+    premium_color: string;
+}
+export interface VenueStyleMap {
+    default_icon: string;
+    default_color: string;
+    gallery: VenueCategoryStyle;
+    business: VenueCategoryStyle;
+    institution: VenueCategoryStyle;
+    popup: VenueCategoryStyle;
+    public: VenueCategoryStyle;
+    studios: VenueCategoryStyle;
+}
+declare module '@material-ui/core/styles/createMuiTheme' {
+    interface Theme {
+        fontFamily: {
+            default: string;
+            accent: string;
+        };
+        appBarHeight: {
+            default: number;
+            compact: number;
+        };
+        globalMenuWidth: number;
+        gutterSize: number;
+        venues: VenueStyleMap;
+    }
+    interface ThemeOptions {
+        fontFamily: {
+            default: string;
+            accent: string;
+        };
+        appBarHeight: {
+            default: number;
+            compact: number;
+        };
+        globalMenuWidth: number;
+        gutterSize: number;
+        venues: VenueStyleMap;
+    }
+}
+declare module '@material-ui/core/styles/createPalette' {
+    interface Palette {
+        globalHeader: PaletteColor;
+    }
+    interface PaletteOptions {
+        globalHeader: PaletteColor;
+    }
+}
 declare const _default: import("@material-ui/core/styles").Theme;
 export default _default;
--- src/theming/theme.tsx
+++ src/theming/theme.tsx
@@ -4,6 +4,51 @@ import { blue, pink } from './colors';
 
 const defaultFontFamily = '"Roboto", "Helvetica", "Arial", sans-serif';
 const accentFontFamily = '"Bryant", "Helvetica", "Arial", sans-serif';
+export interface VenueCategoryStyle {
+  default_icon: string;
+  premium_icon: string;
+  default_color: string;
+  premium_color: string;
+}
+
+export interface VenueStyleMap {
+  default_icon: string;
+  default_color: string;
+  gallery: VenueCategoryStyle;
+  business: VenueCategoryStyle;
+  institution: VenueCategoryStyle;
+  popup: VenueCategoryStyle;
+  public: VenueCategoryStyle;
+  studios: VenueCategoryStyle;
+}
+
+// Extend the Mui types for our customizations
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface Theme {
+    fontFamily: { default: string; accent: string };
+    appBarHeight: { default: number; compact: number };
+    globalMenuWidth: number;
+    gutterSize: number;
+    venues: VenueStyleMap;
+  }
+  // allow configuration using `createMuiTheme`
+  interface ThemeOptions {
+    fontFamily: { default: string; accent: string };
+    appBarHeight: { default: number; compact: number };
+    globalMenuWidth: number;
+    gutterSize: number;
+    venues: VenueStyleMap;
+  }
+}
+
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    globalHeader: PaletteColor;
+  }
+  interface PaletteOptions {
+    globalHeader: PaletteColor;
+  }
+}
 
 export default createMuiTheme({
   // Typography
